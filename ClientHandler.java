369707693430548f52545a2a8f5a4355ab218926
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2021-02-02 20:57:27 +0000

    As above

--- ClientHandler.java
+++ ClientHandler.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import org.json.simple.*;
 
 public class ClientHandler extends Thread {
         private Socket client;
